0d81affa56ade89fdef75b56766bb5d76f68b3a8
Curtis Huttenhower <chuttenh@hsph.harvard.edu>
2013-01-31 09:47:39 -0500

    Commit current HUMAnN incomplete gamma code

--- src/pathway.py
+++ src/pathway.py
@@ -4,34 +4,31 @@ import math
 import re
 import sys
 
-# Implementation thanks to http://www.crbond.com/math.htm thanks to Zhang and Jin
-# Modified to only return normalized/regularized lower incomplete gamma
-def incomplete_gamma( dA, dX ):
+# Adapted from samtools; will be occasionally inaccurate due to iteration stoppage
+def incomplete_gamma( dS, dZ ):
+	
+	dS, dZ = (float(d) for d in (dS, dZ))
+	dSum = dX = 1
+	for i in xrange( 1, 10000 ):
+		dX *= dZ / ( dS + i )
+		dSum += dX
+		if ( dX / dSum ) < 1e-14:
+			break
+	return ( math.exp( ( dS * math.log( dZ ) ) - dZ - _log_gamma( dS + 1 ) + math.log( dSum ) ) if dZ else 0 )
 
-	if ( dA < 0 ) or ( dX < 0 ):
-		return None
-	if not dX:
-		return 0
-	xam = -dX + dA * math.log( dX )
-	if ( xam > 700 ) or ( dA > 170 ):
-		return None
-	if dX <= ( dA + 1 ):
-		r = s = 1.0 / dA
-		for k in range( 1, 61 ):
-			r *= float(dX) / ( dA + k )
-			s += r
-			if abs( r / s ) < 1e-15:
-				break
-		ga = math.gamma( dA )
-		gin = math.exp( xam ) * s
-		return ( gin / ga )
-
-	t0 = 0
-	for k in range( 60, 0, -1 ):
-		t0 = float(k - dA) / ( 1 + ( float(k) / ( dX + t0 ) ) )
-	gim = math.exp( xam ) / ( dX + t0 )
-	ga = math.gamma( dA )
-	return ( 1 - ( gim / ga ) )
+def _log_gamma( dZ ):
+	
+	dX = 0
+	dX += 0.1659470187408462e-06 / ( dZ + 7 )
+	dX += 0.9934937113930748e-05 / ( dZ + 6 )
+	dX -= 0.1385710331296526     / ( dZ + 5 )
+	dX += 12.50734324009056      / ( dZ + 4 )
+	dX -= 176.6150291498386      / ( dZ + 3 )
+	dX += 771.3234287757674      / ( dZ + 2 )
+	dX -= 1259.139216722289      / ( dZ + 1 )
+	dX += 676.5203681218835      / dZ
+	dX += 0.9999999999995183;
+	return ( math.log( dX ) - 5.58106146679532777 - dZ + ( ( dZ - 0.5 ) * math.log( dZ + 6.5 ) ) )
 
 def chi2cdf( dX, dK ):
 	
@@ -60,7 +57,7 @@ class CPathway:
 		def _mean( self, ad ):
 	
 	#		return ( ( reduce( lambda dProd, d: dProd * d, ad, 1 ) ** ( 1.0 / len( ad ) ) ) if ad else 0 )
-			return ( ( len( ad ) / sum( ( 1.0 / d ) for d in ad ) ) if ( ad and min( ad ) ) else 0 )
+			return ( ( len( ad ) / sum( ( 1.0 / d ) for d in ad ) ) if ( ad and ( min( ad ) > 0 ) ) else 0 )
 		
 		def _reqopt( self, adReq, adOpt ):
 	
